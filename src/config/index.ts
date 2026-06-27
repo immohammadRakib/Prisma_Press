857643faa814e01a28894d15755b2d2e88e45350
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-06-28 01:41:38 +0600

    env-setup

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import path from "path";
+
+
+
+dotenv.config({path: path.join(process.cwd(), '.env')});
+
+export default {
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL || "",
+    app_url: process.env.APP_URL,
+    bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET || "",
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || "",
+    jwt_access_expiration: process.env.JWT_ACCESS_EXPIRATION || "15m",
+    jwt_refresh_expiration: process.env.JWT_REFRESH_EXPIRATION || "7d",
+}
