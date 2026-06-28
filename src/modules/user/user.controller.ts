2b0ee1fc8beae97d6ce01a3bde5369c5839287b9
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-06-28 19:12:32 +0600

    commit

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,17 +7,25 @@ import { userService } from "./user.service";
 
 
 const registerUser = async (req: Request, res: Response) => {
-    const payload = req.body;
+    try{
+        const payload = req.body;
     
-    const user  = await userService.registerUserIntoDB(payload);
+        const user  = await userService.registerUserIntoDB(payload);
      
-    res.status(httpStatus.CREATED).json({ 
-    message: 'User registered successfully',
-    data: {
-        user
-    }
+        res.status(httpStatus.CREATED).json({ 
+        message: 'User registered successfully',
+        data: {
+            user
+        }
 
-})}
+})
+    } catch (error) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            message: 'Error registering user',
+            error: (error as Error).message
+        });
+    }
+}
 
 
 export const userController = {
